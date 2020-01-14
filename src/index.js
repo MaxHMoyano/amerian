d1252c452c33d21a17de1485883b12b5b9827a0a
Max Moyano <maximiliano.moyano@corbisstudio.com>
2020-01-14 17:30:08 -0300

    partial navigation and user auth

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { Provider } from "react-redux";
 import configureStore from "./redux/store";
-import "./styles.scss";
+import "./styles/main.scss";
 
 ReactDOM.render(
   <Provider store={configureStore()}>
--- src/routes/humanResources/Staff.js
+++ src/routes/humanResources/Staff.js
@@ -1,7 +1,53 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { Table, Form } from "react-bootstrap";
+// import Select from "react-select";
 
 const Staff = () => {
-  return <div>Staff</div>;
+  // const dummyFilter = ["Option1", "Option 2"];
+
+  return (
+    <Fragment>
+      <div className="d-flex mb-4 align-items-center w-75">
+        <button type="button" className="btn btn-secondary is-rounded mr-3">
+          Agregar Staff
+        </button>
+        <Form.Group controlId="formBasicEmail" className="mr-3">
+          <Form.Control type="text" placeholder="Buscar" />
+        </Form.Group>
+        {/* <Select options="dummyFilter"></Select> */}
+        <button className="btn btn-link">Más filtros</button>
+      </div>
+      <Table striped hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Username</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td colSpan="2">Larry the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </Table>
+    </Fragment>
+  );
 };
 
 export default Staff;
